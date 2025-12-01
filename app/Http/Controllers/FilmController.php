08c232182f87bdc9624462a1715a9ea7a2aad5b2
Raimond <raimond90nin@gmail.com>
2025-12-01 18:19:40 +0100

    frist commit

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -6,17 +6,11 @@ use Illuminate\Support\Facades\Storage;
 class FilmController extends Controller
 {
 
-    /**
-     * Read films from storage
-     */
     public static function readFilms(): array {
         $films = Storage::json('/public/films.json');
         return $films;
     }
-    /**
-     * List films older than input year 
-     * if year is not infomed 2000 year will be used as criteria
-     */
+    
     public function listOldFilms($year = null)
     {        
         $old_films = [];
@@ -33,10 +27,7 @@ class FilmController extends Controller
         }
         return view('films.list', ["films" => $old_films, "title" => $title]);
     }
-    /**
-     * List films younger than input year
-     * if year is not infomed 2000 year will be used as criteria
-     */
+    
     public function listNewFilms($year = null)
     {
         $new_films = [];
@@ -52,9 +43,7 @@ class FilmController extends Controller
         }
         return view('films.list', ["films" => $new_films, "title" => $title]);
     }
-    /**
-     * Lista TODAS las películas o filtra x año o categoría.
-     */
+ 
     public function listFilms($year = null, $genre = null)
     {
         $films_filtered = [];
@@ -62,11 +51,9 @@ class FilmController extends Controller
         $title = "Listado de todas las pelis";
         $films = FilmController::readFilms();
 
-        //if year and genre are null
         if (is_null($year) && is_null($genre))
             return view('films.list', ["films" => $films, "title" => $title]);
 
-        //list based on year or genre informed
         foreach ($films as $film) {
             if ((!is_null($year) && is_null($genre)) && $film['year'] == $year){
                 $title = "Listado de todas las pelis filtrado x año";
--- resources/views/films/list.blade.php
+++ resources/views/films/list.blade.php
@@ -3,9 +3,13 @@
 @if(empty($films))
     <FONT COLOR="red">No se ha encontrado ninguna película</FONT>
 @else
+    <!-- Contador de pelis -->
+    <h3>Total de películas: {{ count($films) }}</h3>
+
     <div align="center">
     <table border="1">
         <tr>
+            <!-- Encabezados de la tabla -->
             @foreach($films as $film)
                 @foreach(array_keys($film) as $key)
                     <th>{{$key}}</th>
@@ -14,14 +18,17 @@
             @endforeach
         </tr>
 
+        <!-- Filas de la tabla -->
         @foreach($films as $film)
             <tr>
                 <td>{{$film['name']}}</td>
                 <td>{{$film['year']}}</td>
                 <td>{{$film['genre']}}</td>
-                <td><img src={{$film['img_url']}} style="width: 100px; heigth: 120px;" /></td>
+                <td>{{$film['country']}}</td>
+                <td>{{$film['duration']}}</td>
+                <td><img src={{$film['img_url']}} style="width: 100px; height: 120px;" /></td>
             </tr>
         @endforeach
     </table>
-</div>
+    </div>
 @endif
