24dc516414052e71342915e1ed4c3076a11ff1ea
RAIMOND <raimond90nin@gmail.com>
2026-03-13 18:59:03 +0100

    Revert "list-actors"

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,8 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'validate_year' => \App\Http\Middleware\ValidateYear::class,
-        'year' => \App\Http\Middleware\Year::class,
+        'year' => \App\Http\Middleware\ValidateYear::class,
         'validateurl' => \App\Http\Middleware\ValidateUrl::class
     ];
 }
